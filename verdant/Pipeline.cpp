9c703f59aa2d88093b073b7033d5781265b3cc21
Toby Chen <chenyanjun912@hotmail.com>
2022-07-11 01:19:04 -0700

    Multithreaded and upgraded Pipeline

--- verdant/Pipeline.cpp
+++ verdant/Pipeline.cpp
@@ -1,46 +1,65 @@
 #include "Pipeline.h"
+#include "Integrator.h"
+#include "PathIntegrator.h"
+#include "Sampler.h"
+#include "TaskQueue.h"
+#include <memory>
+#include <stdio.h>
+#include <thread>
+#include <vector>
 
 namespace verdant {
 PathTracePipeline::PathTracePipeline(unsigned int width, unsigned int height,
                                      unsigned int samples)
     : samples(samples) {
   scene = std::make_shared<Scene>();
-  integrator = std::make_shared<PathTraceIntegrator>();
   camera = std::make_shared<Camera>();
   film = std::make_shared<Film>(width, height);
 
   camera->set_aspect_ratio(film->get_aspect_ratio());
   camera->set_fov_y(M_PI / 180.f * 90.f);
-
-  scene->add_point_light({2.5f, 0.0f, 2.5f}, {1.0f, 3.0f, 5.0f});
 }
 
 void PathTracePipeline::run() {
-  auto total_film =
-      std::make_shared<Film>(film->get_width(), film->get_height());
+  const unsigned int tile_len = 256;
 
-  unsigned int d;
-  unsigned int x, y, t;
-  for (d = 1; d <= 4; d++) {
-    integrator->set_only_depth(d);
-    film->clear();
+  unsigned int y, x;
+  std::vector<const Task *> tasks;
+  for (y = 0; y < film->get_height(); y += tile_len) {
+    for (x = 0; x < film->get_width(); x += tile_len) {
+      tasks.push_back(TaskQueue::default_queue().enqueue(
+          [this, x, y, tile_len]() { render_tile(x, y, tile_len, tile_len); }));
+    }
+  }
+  TaskQueue::default_queue().enqueue_await_all(
+      [this]() { film->write_to_ppm("hello.ppm"); }, tasks);
+}
 
-    for (y = 0; y < film->get_height(); y++) {
-      for (x = 0; x < film->get_width(); x++) {
-        Ray ray = camera->generate_ray_from_uv(film->xy_to_uv(x, y));
-        ray.origin.z() += 5.0f;
-        for (t = 0; t < samples; t++) {
-          float3 Li = integrator->Lo_from_ray(*scene, ray);
-          film->average_radiance(x, y, Li);
-        }
-        total_film->accumulate_radiance(x, y, film->get_radiance(x, y));
+void PathTracePipeline::single_pixel(unsigned int x, unsigned int y) {
+  UniformSampler sampler;
+  PathIntegrator integrator(*scene, sampler);
+
+  Ray ray = camera->generate_ray_from_uv(film->xy_to_uv(x, y));
+  ray.origin.z() += 5.0f;
+  float3 Li = integrator.Lo_from_ray(ray);
+  film->average_radiance(x, y, Li);
+}
+
+void PathTracePipeline::render_tile(unsigned int x_begin, unsigned int y_begin,
+                                    unsigned int x_len, unsigned int y_len) {
+  UniformSampler sampler;
+  PathIntegrator integrator(*scene, sampler);
+  unsigned int x, y;
+  for (y = y_begin; y < y_begin + y_len && y < film->get_height(); y++) {
+    for (x = x_begin; x < x_begin + x_len && x < film->get_width(); x++) {
+      unsigned int t;
+      Ray ray = camera->generate_ray_from_uv(film->xy_to_uv(x, y));
+      ray.origin.z() += 5.0f;
+      for (t = 0; t < samples; t++) {
+        float3 Li = integrator.Lo_from_ray(ray);
+        film->average_radiance(x, y, Li);
       }
     }
-
-    char file_name[256];
-    sprintf(file_name, "hello%d.ppm", d);
-    film->write_to_ppm(file_name);
   }
-  total_film->write_to_ppm("hello.ppm");
 }
 } // namespace verdant
--- verdant/Pipeline.h
+++ verdant/Pipeline.h
@@ -12,18 +12,19 @@ public:
                     unsigned int samples);
 
   std::shared_ptr<Scene> get_scene() const { return scene; }
-  std::shared_ptr<PathTraceIntegrator> get_integrator() const {
-    return integrator;
-  }
   std::shared_ptr<Camera> get_camera() const { return camera; }
   std::shared_ptr<Film> get_film() const { return film; }
 
   void run();
+  void single_pixel(unsigned int x, unsigned int y);
+
+protected:
+  void render_tile(unsigned int x, unsigned int y, unsigned int x_len,
+                   unsigned int y_len);
 
 private:
   unsigned int samples;
   std::shared_ptr<Scene> scene;
-  std::shared_ptr<PathTraceIntegrator> integrator;
   std::shared_ptr<Camera> camera;
   std::shared_ptr<Film> film;
 };

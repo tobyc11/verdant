40b5b0c74beb178e563d2bf047c7465330cf3d61
Toby Chen <chenyanjun912@hotmail.com>
2022-07-11 01:18:21 -0700

    Material reflection and refraction

--- verdant/Material.cpp
+++ verdant/Material.cpp
@@ -1,5 +1,6 @@
 #include "Material.h"
 #include "BRDF.h"
+#include "Sampler.h"
 #include <memory>
 
 namespace verdant {
@@ -9,5 +10,110 @@ std::shared_ptr<Material> Material::create_lambert(float3 c) {
   return m;
 }
 
-float3 Material::f() const { return F_lambert(c); }
+std::shared_ptr<Material> Material::create_reflect(float3 c, float eta) {
+  std::shared_ptr<Material> m(new Material(MaterialKind::Reflect));
+  m->c = c;
+  m->etaI = 1.0f;
+  m->etaT = eta;
+  return m;
+}
+
+std::shared_ptr<Material> Material::create_refract(float3 c, float eta) {
+  std::shared_ptr<Material> m(new Material(MaterialKind::Refract));
+  m->c = c;
+  m->etaI = 1.0f;
+  m->etaT = eta;
+  return m;
+}
+
+std::shared_ptr<Material> Material::create_glass(float3 c, float eta) {
+  std::shared_ptr<Material> m(new Material(MaterialKind::Glass));
+  m->c = c;
+  m->etaI = 1.0f;
+  m->etaT = eta;
+  return m;
+}
+
+std::shared_ptr<Material> Material::create_specular(float3 c, float eta) {
+  std::shared_ptr<Material> m(new Material(MaterialKind::Specular));
+  m->c = c;
+  m->etaI = 1.0f;
+  m->etaT = eta;
+  return m;
+}
+
+float3 Material::f(const float3 &L, const float3 &V) const {
+  if (is_delta()) {
+    return float3::ZERO;
+  }
+  return F_lambert(c);
+}
+
+inline bool refract(const float3 &V, const float3 &N, float eta, float3 &T) {
+  // Based on the pbrt implementation
+  float cosThetaI = std::abs(dot(V, N)); // abs??
+  float sin2ThetaI = std::max(0.f, 1.f - cosThetaI * cosThetaI);
+  float sin2ThetaT = eta * eta * sin2ThetaI;
+  if (sin2ThetaT >= 1) {
+    return false;
+  }
+  float cosThetaT = std::sqrt(1 - sin2ThetaT);
+
+  T = eta * -V + (eta * cosThetaI - cosThetaT) * N;
+  T = normalize(T);
+  return true;
+}
+
+float3 Material::sample_f(UniformSampler &sampler, const float3 &V, float3 &L,
+                          float &pdf) const {
+  pdf = 1.0f;
+  switch (kind) {
+  case MaterialKind::Lambert:
+    return sample_f_lambert(sampler, V, L, pdf);
+  case MaterialKind::Reflect:
+    return sample_f_reflection(sampler, V, L, pdf);
+  case MaterialKind::Refract:
+    return sample_f_refraction(sampler, V, L, pdf);
+  case MaterialKind::Glass:
+    return sum_f(sampler, V, L, pdf, &Material::sample_f_reflection,
+                 &Material::sample_f_refraction);
+  case MaterialKind::Specular:
+    return sum_f(sampler, V, L, pdf, &Material::sample_f_reflection,
+                 &Material::sample_f_lambert);
+  }
+}
+
+float3 Material::sample_f_lambert(UniformSampler &sampler, const float3 &V,
+                                  float3 &L, float &pdf) const {
+  CosineWeightedHemisphereDistribution dist;
+  auto [pdf2, L2] = dist.sample(sampler);
+  L = L2;
+  pdf *= pdf2;
+  return F_lambert(c);
+}
+
+float3 Material::sample_f_reflection(UniformSampler &sampler, const float3 &V,
+                                     float3 &L, float &pdf) const {
+  // Specular reflection
+  L = float3(-V.x(), -V.y(), V.z());
+  float cosThetaT = std::abs(L.z());
+  return c * Fr_dielectric(cosThetaT, etaI, etaT) / cosThetaT;
+}
+
+float3 Material::sample_f_refraction(UniformSampler &sampler, const float3 &V,
+                                     float3 &L, float &pdf) const {
+  // Specular transmission
+  bool entering = V.z() > 0.0f;
+  float etaA = entering ? etaI : etaT;
+  float etaB = entering ? etaT : etaI;
+
+  bool ok = refract(V, float3(0, 0, 1), etaA / etaB, L);
+  if (!ok) {
+    // Total internal reflection occured, no refraction possible
+    return float3::ZERO;
+  }
+
+  float cosThetaT = std::abs(L.z());
+  return c * (1 - Fr_dielectric(cosThetaT, etaA, etaB)) / cosThetaT;
+}
 } // namespace verdant
--- verdant/Material.h
+++ verdant/Material.h
@@ -1,21 +1,59 @@
 #pragma once
 #include "MathDefs.h"
+#include "Sampler.h"
 #include <memory>
 
 namespace verdant {
-enum class MaterialKind { Lambert };
+enum class MaterialKind { Lambert, Reflect, Refract, Glass, Specular };
 
 class Material {
 public:
   // Creates a Lambertian material with diffuse reflectance c
   static std::shared_ptr<Material> create_lambert(float3 c);
+  static std::shared_ptr<Material> create_reflect(float3 c, float eta);
+  static std::shared_ptr<Material> create_refract(float3 c, float eta);
+  static std::shared_ptr<Material> create_glass(float3 c, float eta);
+  static std::shared_ptr<Material> create_specular(float3 c, float eta);
 
-  float3 f() const;
+  // Both L and V are in BRDF coordinates, with normal being (0, 0, 1)
+  float3 f(const float3 &L, const float3 &V) const;
+
+  // Sample an incoming direction
+  float3 sample_f(UniformSampler &sampler, const float3 &V, float3 &L,
+                  float &pdf) const;
+
+  bool is_delta() const {
+    return kind == MaterialKind::Reflect || kind == MaterialKind::Refract ||
+           kind == MaterialKind::Glass || kind == MaterialKind::Specular;
+  }
+
+protected:
+  float3 sample_f_lambert(UniformSampler &sampler, const float3 &V, float3 &L,
+                          float &pdf) const;
+  float3 sample_f_reflection(UniformSampler &sampler, const float3 &V,
+                             float3 &L, float &pdf) const;
+  float3 sample_f_refraction(UniformSampler &sampler, const float3 &V,
+                             float3 &L, float &pdf) const;
+
+  template <typename T1, typename T2>
+  float3 sum_f(UniformSampler &sampler, const float3 &V, float3 &L, float &pdf,
+               T1 fn1, T2 fn2) const {
+    auto [_pdf, roll] = sampler.sample();
+    const float k = 0.5f;
+    if (roll < k) {
+      pdf = k;
+      return (this->*fn1)(sampler, V, L, pdf);
+    } else {
+      pdf = 1 - k;
+      return (this->*fn2)(sampler, V, L, pdf);
+    }
+  }
 
 private:
   Material(MaterialKind kind) : kind(kind) {}
 
   MaterialKind kind;
   float3 c;
+  float etaI, etaT;
 };
 } // namespace verdant
